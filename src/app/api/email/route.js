8bc34115364421fb29573649edc50e866ce6c191
DehamiDiv <dehamidivyanjali166@gmail.com>
2025-12-17 20:41:57 +0530

    Email Subscription List and Deleting

--- src/app/api/email/route.js
+++ src/app/api/email/route.js
@@ -2,6 +2,7 @@ import { ConnectDB } from "@/lib/config/db.js";
 import EmailModel from "@/lib/models/EmailModel.js";
 import { NextResponse } from "next/server";
 
+/* ---------- POST: Save Email ---------- */
 export async function POST(request) {
   try {
     await ConnectDB();
@@ -23,9 +24,30 @@ export async function POST(request) {
       msg: "Email Subscribed Successfully",
     });
   } catch (error) {
+    console.error(error);
     return NextResponse.json(
       { success: false, msg: "Server Error" },
       { status: 500 }
     );
   }
 }
+
+/* ---------- GET: Fetch Emails ---------- */
+export async function GET() {
+  try {
+    await ConnectDB();
+    const emails = await EmailModel.find({});
+    return NextResponse.json({ success: true, emails });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { success: false, msg: "Server Error" },
+      { status: 500 }
+    );
+  }
+}
+ export async function DELETE(request){
+  const id = await request.nextUrl.searchParams.get("id");
+  await EmailModel.findByIdAndDelete(id);
+  return NextResponse.json({success:true,msg:"Email Deleted"})
+ }
